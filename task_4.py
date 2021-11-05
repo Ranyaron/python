154849dc171a00d5d6625b40d61e8893685f6081
Ilia <51255275+Ranyaron@users.noreply.github.com>
2021-11-05 09:01:23 +0300

    Delete task_4.py

--- task_4.py
+++
@@ -1,6 +0,0 @@
-"""Выводит массив чисел, не имеющие повторений."""
-
-my_list = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
-new_list = [el for el in my_list if my_list.count(el) < 2]
-
-print(new_list)
