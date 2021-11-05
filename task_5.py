7a4c5a23996173fcf7a9993348bb81c590219f9b
Ilia <51255275+Ranyaron@users.noreply.github.com>
2021-11-05 09:01:39 +0300

    Delete task_5.py

--- task_5.py
+++
@@ -1,9 +0,0 @@
-from functools import reduce
-
-
-def my_func(prev_el, el):
-    """Возвращает результат произведения элементов списка."""
-    return prev_el + el
-
-
-print(reduce(my_func, [el for el in range(100, 1000 + 1)]))
